dc98483d80a21b6d7eca52bfe2a92b7b4b82c09a
Ben Klang <bklang@mojolingo.com>
2015-09-18 08:15:58 -0400

    Update TalkingStick

--- db/migrate/20150918121441_add_slug_to_talking_stick_rooms.talking_stick.rb
+++ db/migrate/20150918121441_add_slug_to_talking_stick_rooms.talking_stick.rb
@@ -1,6 +1,12 @@
 # This migration comes from talking_stick (originally 20150722200822)
 class AddSlugToTalkingStickRooms < ActiveRecord::Migration
-  def change
+  def up
     add_column :talking_stick_rooms, :slug, :string
+    # Add slugs to the existing rooms
+    TalkingStick::Room.all.map &:save
+  end
+
+  def down
+    remove_column :talking_stick_rooms, :slug
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150917184049) do
+ActiveRecord::Schema.define(version: 20150918121441) do
 
   create_table "talking_stick_participants", force: :cascade do |t|
     t.string   "name",       limit: 255
